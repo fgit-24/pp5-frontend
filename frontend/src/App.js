3c8d4e567aaa40e9c8d09c72922fe77069fb1050
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-01 21:30:46 +0100

    Add FirstComponent

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <h1>Welcome to ReactJS and Drf</h1>;
+}
+
+export default App;
---
+++ frontend/src/components/SecondComponent.js
@@ -0,0 +1,12 @@
+import FirstComponent from "./components/FirstComponent";
+
+function FirstComponent() {
+  return (
+    <>
+      <h1>Welcometo ReactJ@ and DRF</h1>;
+      <FirstComponent />;
+    </>
+  );
+}
+
+export default FirstComponent;
