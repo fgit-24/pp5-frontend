c520a11063fb0c030dc4dcc4e4b73018822eeb0c
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-02 08:50:23 +0100

    Build onChange functions for login form

--- frontend/src/components/Forms.js
+++ frontend/src/components/Forms.js
@@ -1,19 +1,36 @@
 import { Component } from "react";
 
 class Forms extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
   render() {
     return (
       <div className="container">
         <input
           type="text"
+          value={this.state.username}
+          onChange={(e) => {
+            this.state.setState({ username: e.target.value });
+          }}
           className="form-control"
           placeholder="Please enter name"
         ></input>
         <input
           type="text"
           className="form-control"
+          value={this.state.password}
+          onChange={(e) => {
+            this.state.setState({ password: e.target.value });
+          }}
           placeholder="Please enter password"
         ></input>
+        <button className="btn btn-primary">Login</button>
       </div>
     );
   }
