12ecae2320e8e4fd94d1a3517aba679f5c0f06fd
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-02 17:49:21 +0100

    Utilize useEffect to fetch data

--- frontend/src/components/FetchData.js
+++ frontend/src/components/FetchData.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function FetchData() {
-  return <div>FetchData</div>;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((resp) => resp.json())
+      .then((result) => {
+        console.log(result);
+      });
+  }, []);
+
+  return;
+  <>
+    <FetchData />
+  </>;
 }
 
 export default FetchData;
