3cd6f1fa3b2d589012af964ba19f66ee54c2d95b
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-02 12:19:09 +0100

    Implement decrease and increase button with useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,15 +9,14 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import React from "react";
 import ComponentA from "./components/ComponentA";
+import Counter from "./components/Counter";
 
 export const MyContext = React.createContext();
 
 function App() {
   return (
     <>
-      <MyContext.Provider value="This is the data for context">
-        <ComponentA />
-      </MyContext.Provider>
+      <Counter />
     </>
   );
 }
--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -1,8 +1,38 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increase = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  decrease = () => {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <h2>{this.state.count}</h2>
+        <button onClick={this.increase} className="btn btn-primary">
+          Increase
+        </button>
+        <button onClick={this.decrease} className="btn btn-primary">
+          Decrease
+        </button>
+      </div>
+    );
   }
 }
 
