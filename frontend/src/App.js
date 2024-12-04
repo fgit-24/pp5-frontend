625e7e5b5c4e02fad8adf7d9423ed52a997b47c5
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-04 12:52:13 +0100

    fetch articles (connected)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,27 +3,32 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [articles, setArticles] = useState(["Article One", "Article Two"]);
+  const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/", {
+    fetch("http://127.0.0.1:8000/articles/", {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
-        Authorization: "35d93ade08367b58d87f74bff1d3374087b265f2",
+        "Content-Type" : "application/json",
+        "Authorization" : "Token 35d93ade08367b58d87f74bff1d3374087b265f2",
       },
     })
       .then((resp) => resp.json())
       .then((result) => {
-        console.log(result);
+        setArticles(result)
       })
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <div>
-      {articles.map((article) => {
-        return <h2>{article}</h2>;
+      {articles.map(article => {
+        return (
+        <div>
+          <h2>{article.title}</h2>
+          <p>{article.description}</p>
+        </div>
+        )
       })}
     </div>
   );
