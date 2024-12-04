fc8462593c2687fad170de6de8ef7a1ac27b0531
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-04 23:07:15 +0100

    Set up useState for login error

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,8 +3,23 @@ import React, {useState} from 'react'
 function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
 
-
+    const login = () => {
+        fetch('http://127.0.0.1:8000/rest-auth/login', {
+            method: "POST",
+            headers: {
+              "Content-Type" : "application/json",
+            },
+            body:JSON.stringify({username, password})
+        })
+        .then(resp => resp.json())
+        .then(result => {
+            if(result.key == undefined){
+                setError("Invalid username or password")
+                return
+        }
+    })
 
     return (
         <div className='container mt-4'>
