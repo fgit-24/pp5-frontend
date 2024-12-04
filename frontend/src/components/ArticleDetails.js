edd8c3fa2f3a4afdd604afd01baacb6121c18a25
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-04 19:12:15 +0100

    Build useEffect, to fetch data for ArticleDetails and unse params for slug

--- frontend/src/components/ArticleDetails.js
+++ frontend/src/components/ArticleDetails.js
@@ -1,6 +1,21 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
 
-function ArticleDetails = () => {
+function ArticleDetails() {
+    const params = useParamas()
+    const [article, setArticle] = useState({})
+    
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/articles/", {
+            method: "GET",
+            headers: {
+              "Content-Type" : "application/json",
+              "Authorization" : "Token 35d93ade08367b58d87f74bff1d3374087b265f2",
+            },
+          })
+    }, [])
+
   return (
     <div></div>
   )
