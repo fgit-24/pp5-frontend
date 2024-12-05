61a6b0b1508b34ddefdd34f9d02249aabdb03551
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-05 11:18:26 +0100

    Building AddArticle Component

---
+++ frontend/src/components/AddArticle.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+
+function AddArticle() {
+    
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [error, setError] = useState('')
+
+    return (
+        <div className='container mt-5'>
+
+            <h2>Add Article</h2>
+            <div className='mb-3'>
+                <input type='text' className='form-control'
+                placeholder='Please Enter a Title'
+                value={title}
+                onChange={evt = evt.target.value}
+                />
+            </div>
+            
+            <div className='mb-3'>
+                <textarea className='form-control'
+                value = {description}
+                onChange = {evt => setDescription(evt.target.value)}
+
+            />
+
+            </div>
+
+        <div className='mb-3'>
+            <button className = "btn btn-success">Add Article</button>
+        </div>
+        
+    </div>
+    )
+}
+
+export default AddArticle
