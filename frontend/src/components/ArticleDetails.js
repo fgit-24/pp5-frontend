2681ebbf2c8de6570480567dce739568601d7a2b
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-05 19:01:14 +0100

    Protect update and delete button in ArticleDetails.js

--- frontend/src/components/ArticleDetails.js
+++ frontend/src/components/ArticleDetails.js
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 function ArticleDetails() {
     const params = useParams()
     const [article, setArticle] = useState({})
+    const [req, setReq] = useState('')
     
     const token = localStorage.getItem('mytoken')
 
@@ -20,7 +21,22 @@ function ArticleDetails() {
           .then(resp => resp.json())
           .then(result => setArticle(result))
           .catch(error => console.log(error))
-    }, [params.slug])
+    }, [params.slug, token])
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:8000/dj-rest-auth/user/', {
+        method: "GET",
+        headers: {
+          "Content-Type" : "application/json",
+          "Authorization" : `Token ${token}`,
+        },
+      })
+      .then(resp => resp.json())
+      .then(result => setReq(result))
+      .catch(error => console.log(error))
+
+    }, [token])
+
 
   return (
     <div className="container">
@@ -32,6 +48,15 @@ function ArticleDetails() {
       <br/>
 
       <p>{article.description}</p>
+      {req.username === article.author ?
+      <div>
+      <button className='btn btn-danger mx-3 mt-3'>Delete</button>
+      <button className='btn btn-success mx-3 mt-3'>Update</button>
+      </div>
+      :
+
+      null
+      }
     </div>
   );
 }
