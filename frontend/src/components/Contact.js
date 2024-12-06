ca810bcef2f70211c0942fcacc9d9f4adabe5978
Florian-Haas <fhaasbusiness@gmail.com>
2024-12-06 11:46:44 +0100

    Implement necessary useStates in contact component

---
+++ frontend/src/components/Contact.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const ContactForm = () => {
+const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+
+
+}
